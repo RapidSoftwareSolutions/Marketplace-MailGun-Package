7403471c5acd8d8361af726ed638dfaabdc132ab
Dima Shirokov <dshirokoff@ukr.net>
2016-09-13 18:51:29 +0300

    /sendEmail without attach

--- api/send_email.js
+++ api/send_email.js
@@ -1,6 +1,6 @@
 const _ = require('../lib/functions');
 const mailgun = require('mailgun-js');
-const apiArgs = ['o:tag', 'o:campaign', 'o:dkim', 'o:deliverytime', 'o:testmode', 'o:tracking', 'o:tracking-clicks', 'o:tracking-opens', 'o:require-tls', 'o:skip-verification', 'h:X-My-Header', 'v:my-var'];
+const apiArgs = ['html', 'o:tag', 'o:campaign', 'o:dkim', 'o:deliverytime', 'o:testmode', 'o:tracking', 'o:tracking-clicks', 'o:tracking-opens', 'h:X-My-Header', 'v:my-var'];
 
 module.exports = (req, res) => {
 
@@ -18,15 +18,14 @@ module.exports = (req, res) => {
         bcc, 
         subject,
         text, 
-        html, 
         inline,
         to = "to",
-        'o:require-tls':       oRequireTls       = 'False',
-        'o:skip-verification': oSkipVerification = 'False', 
+        'o:require-tls': oRequireTls,
+        'o:skip-verification': oSkipVerification 
     } 
         = req.body.args;
 
-    if(!apiKey || !domain || !mFrom || !mTo) {
+    if(!apiKey || !mFrom || !mTo) {
         _.echoBadEnd(r, to, res);
         return;
     }
@@ -41,23 +40,33 @@ module.exports = (req, res) => {
     if(inline && _.checkUrl(inline)) inline = request(inline);
 
      /* MailGun SDK Initialization */
-    mailgun({apiKey: apiKey, domain: domain});
-
+    let mail = mailgun({apiKey: apiKey, domain: domain});
 
     let data = {
-        mFrom: from,
-        mTo: to,
+        from: mFrom,
+        to: mTo,
         subject: subject,
         text: text,
-        html: html,
-        'o:require-tls': oRequireTls,
-        'o:skip-verification': oSkipVerification
+        'o:require-tls': oRequireTls || "False",
+        'o:skip-verification': oSkipVerification || "False",
         //attachment: inline || new mailgun.Attachment({data: file, filename: filename})
     };
 
-
     Object.keys(req.body.args).forEach((key) => {
-        if( apiArgs.indexOf(req.body.args[key]) != -1 && req.body.args[key] ) 
-            data[key] = apiKey[key];
+        if( apiArgs.indexOf(key) !== -1 && req.body.args[key] )
+            data[key] = req.body.args[key];
+    });
+
+    mail.messages().send(data, (err, body) => {
+        if(err) {
+            r.contextWrites[to] = JSON.stringify(err);
+            r.callback = 'error';
+        }
+        else {
+            r.contextWrites[to] = JSON.stringify(body);
+            r.callback = 'success';
+        }
+
+        res.status(200).send(r);
     });
 };
---
+++ test/api.js
@@ -0,0 +1,33 @@
+let assert = require('chai').assert;
+let request = require('supertest-as-promised');
+
+let app = require('../index');
+let apiKey = 'key-8bd332813bcf19eaad7ab28b99397b74',
+    domain = 'sandbox97528515a5a448cc8c395be329aa95a3.mailgun.org',
+    mFrom = 'dima.shirokoff@rapidapi.com',
+    mTo = 'dima.shirokoff@rapidapi.com',
+    to = 'to',
+    subject = 'Test',
+    text = 'hello';
+
+describe('/sendEmail function', () => {
+    it('should send email', () => {
+        return request(app)
+        .post('/api/'+ global.PACKAGE_NAME +'/sendEmail')
+        .send({args: { apiKey, domain, mFrom, mTo, to, subject, text }})
+        .expect(200)
+        .then((data) => {
+            assert.equal(data.body.callback, 'success');
+        });
+    });
+    
+    it('should return error callback', () => {
+        return request(app)
+        .post('/api/'+ global.PACKAGE_NAME +'/sendEmail')
+        .send({args: { apiKey, mFrom, mTo, to, subject, text }})
+        .expect(200)
+        .then((data) => {
+            assert.equal(data.body.callback, 'error');
+        });
+    });
+});
