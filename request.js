349670681407359545f2f37ef020f30e610619bd
Dima Shirokov <dshirokoff@ukr.net>
2016-09-20 11:55:45 +0300

    request

--- request.js
+++ request.js
@@ -2,35 +2,35 @@ const request = require('request');
 
 module.exports = function (apiKey, uri, callback) {
 
-	let response       = {};
-		response.items = [];	
+    let response       = {};
+        response.items = [];    
 
-	(function doRequest(uri, callback) {
+    (function doRequest(uri, callback) {
 
-		request(uri, (err, res, body) => {
-	    	if(err || res.statusCode !== 200) {
-	    		callback(err, res, body);
-	    		return;
-	    	}
-	    	
-	    	let parsed = JSON.parse(body);
+        request(uri, (err, res, body) => {
+            if(err || res.statusCode !== 200) {
+                callback(err, res, body);
+                return;
+            }
+            
+            let parsed = JSON.parse(body);
 
-	    	if(!parsed.items.length) {
-	    		callback(null, res, response);
-	    		return;
-	    	}
+            if(!parsed.items.length) {
+                callback(null, res, response);
+                return;
+            }
 
-	    	for (var i = parsed.items.length - 1; i >= 0; i--) {
-    			response.items.push(parsed.items[i]);
-	    	}
+            for (var i = parsed.items.length - 1; i >= 0; i--) {
+                response.items.push(parsed.items[i]);
+            }
 
-	    	if(parsed.paging && parsed.paging.next) {
-	    		doRequest(parsed.paging.next, callback);
-	    	}
-	    	else 
-				callback(null, res, response);
+            if(parsed.paging && parsed.paging.next) {
+                doRequest(parsed.paging.next, callback);
+            }
+            else 
+                callback(null, res, response);
 
-    	}).auth('api', apiKey);
+        }).auth('api', apiKey);
 
-	})(uri, callback);
+    })(uri, callback);
 }
